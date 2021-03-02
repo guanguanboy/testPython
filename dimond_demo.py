c8f770f7e45252404a1cca84da4c7b33c1add681
liguanlin <517445163@qq.com>
2021-03-02 10:44:20 +0800

    update dimond_demo.py

--- dimond_demo.py
+++ dimond_demo.py
@@ -8,11 +8,12 @@ size = (1, 28, 28)
 *号的作用是分配元组中的数据
 """
 c = b.view(-1, *size)
-print(c.shape)
+print(f"c.shape = {c.shape}") #torch.Size([128, 1, 28, 28])
 
 #上面*size的作用就是将（1， 28，28）转换为1，28， 28
+#因此上面的这句话的作用类似与b.view(-1, 1, 28, 28)
 d = b.view(-1, 1, 28, 28)
-print(d.shape)
+print(f"d.shape={d.shape}")
 
 def fun(*size): #*代表收集参数,作用是将调用时提供的所有值，放在一个元组里。
     print(type(size)) # 这个时候size的类型是<class 'tuple'>
